86854bd13b5cd7844e4e55e0c730221249fdf92b
Juanakas <juanclementeguerrero@outlook.com>
2026-01-24 20:11:41 +0000

    1.Installed gunicorn 2.Created procfile 3.Added ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1'] 4.Changed DEBUG=False

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ew4w)gt_1ab!lau%n+6p=uq_-1!%d!lb)xxkvd3=le@igjdr%5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
